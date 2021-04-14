44b40768b8bef08f139f337ee366218feb2fcc6f
mbeaujar <maelbeaujard2@gmail.com>
2021-04-14 17:06:31 +0200

    adding cmd cd

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mbeaujar <mbeaujar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 20:05:41 by mbeaujar          #+#    #+#             */
-/*   Updated: 2021/04/13 18:40:32 by mbeaujar         ###   ########.fr       */
+/*   Updated: 2021/04/14 17:02:05 by mbeaujar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,13 @@
 #include <stdlib.h>
 #include <fcntl.h> 
 #include "../libft/inc/libft.h"
+#include <limits.h>
+#include <errno.h>
+#include <string.h>
+
+# ifndef PATH_MAX
+#  define PATH_MAX 4096
+# endif 
 
 typedef struct s_env
 {
@@ -28,16 +35,9 @@ typedef struct s_env
 
 void ft_putstr(char *str);
 
-/*
-** prompt
-*/ 
-
-void prompt(void);
+void prompt(t_env *head);
 void display_prompt(void);
 
-/*
-** env
-*/
 
 t_env *fill_env(char **envp);
 void addlstenv(t_env **head, char *env);
@@ -45,6 +45,12 @@ void freelstenv(t_env *head);
 void printlstenv(t_env *head);
 char *return_env_name(char *env);
 char *return_env_value(char *env);
+t_env *search_env(t_env *head, char *env_name);
+
+void change_directory(t_env *head, char *path);
+void cd(t_env *head, char *cmd);
+
+char *get_absolu_path(char *str, size_t size);
 
 
 #endif 
--- source/main.c
+++ source/main.c
@@ -6,32 +6,25 @@
 /*   By: mbeaujar <mbeaujar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 20:05:05 by mbeaujar          #+#    #+#             */
-/*   Updated: 2021/04/13 18:17:31 by mbeaujar         ###   ########.fr       */
+/*   Updated: 2021/04/14 16:53:18 by mbeaujar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
-
 
 // envp = toutes les variable d'environnement
 
 int main(int argc, char **argv, char **envp)
 {
 	t_env *head;
-	char *name;
-	char *value;
-	//prompt();
+	
+	head = NULL;
 	if (argc && argv)
 	{
 		head = fill_env(envp);
+		prompt(head);
 		freelstenv(head);
 	}
-	name = return_env_name(envp[0]);
-	value = return_env_value(envp[0]);
-	printf("name : %s\nvalue : %s\n", name, value);
-	free(name);
-	free(value);
-	//prompt();
 	// head = NULL;
 	// system("leaks minishell");
 	return (0);
