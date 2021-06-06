c4497dc3a6c01ea7a2ad940c9b212d8d1c1aebbf
Mael BEAUJARD <mbeaujar@e2r12p4.42.fr>
2021-06-06 16:35:39 +0200

    ctrl arrow

--- source/prompt/ctrl-arrow.c
+++ source/prompt/ctrl-arrow.c
@@ -6,7 +6,7 @@
 /*   By: mbeaujar <mbeaujar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/06 13:49:37 by mbeaujar          #+#    #+#             */
-/*   Updated: 2021/06/06 16:32:14 by mbeaujar         ###   ########.fr       */
+/*   Updated: 2021/06/06 16:35:35 by mbeaujar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void search_word_left(t_prompt *prompt)
 {
     if (prompt->indice == 0)
         return;
+    if (prompt->buffer->buff[prompt->indice] != ' ' && prompt->buffer->buff[prompt->indice - 1] != ' ')
+        prompt->indice--;
     if (prompt->buffer->buff[prompt->indice] != ' ' && prompt->indice > 0 && prompt->buffer->buff[prompt->indice - 1] != ' ')
     {
         while (prompt->buffer->buff[prompt->indice] != ' ' && prompt->indice > 0)
