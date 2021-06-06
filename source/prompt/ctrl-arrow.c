f79739ff56410f2e63ecb223adb11e4c9a9d0f12
Mael BEAUJARD <mbeaujar@e2r12p4.42.fr>
2021-06-06 16:57:37 +0200

    update bonus

--- source/prompt/ctrl-arrow.c
+++ source/prompt/ctrl-arrow.c
@@ -6,7 +6,7 @@
 /*   By: mbeaujar <mbeaujar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/06 13:49:37 by mbeaujar          #+#    #+#             */
-/*   Updated: 2021/06/06 16:56:11 by mbeaujar         ###   ########.fr       */
+/*   Updated: 2021/06/06 16:57:35 by mbeaujar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,10 +63,8 @@ void search_word_right(t_prompt *prompt)
     while (prompt->buffer->buff[prompt->indice] != ' ' && prompt->indice < prompt->buffer->strlen)
         move_right(prompt);
     while (prompt->buffer->buff[prompt->indice] == ' ' && prompt->indice < prompt->buffer->strlen)
-        move_right(prompt);
-    while (prompt->buffer->buff[prompt->indice] != ' ' && prompt->indice < prompt->buffer->strlen)
     {
-        if (prompt->buffer->buff[prompt->indice + 1] == ' ')
+        if (prompt->buffer->buff[prompt->indice + 1] != ' ')
             break ;
         move_right(prompt);
     }
