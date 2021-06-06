295266af5fcfc87a176e7f5e4154a1761b011a62
Mael BEAUJARD <mbeaujar@e2r12p4.42.fr>
2021-06-06 15:09:55 +0200

    ctrl arrow

--- source/prompt/ctrl-arrow.c
+++ source/prompt/ctrl-arrow.c
@@ -6,7 +6,7 @@
 /*   By: mbeaujar <mbeaujar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/06 13:49:37 by mbeaujar          #+#    #+#             */
-/*   Updated: 2021/06/06 14:42:50 by mbeaujar         ###   ########.fr       */
+/*   Updated: 2021/06/06 15:09:47 by mbeaujar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ void    search_word_left(t_prompt *prompt)
 {
     if (prompt->indice == 0)
         return ;
-    while (prompt->buffer->buff[prompt->indice] == ' ' && prompt->indice - 1 > 0)
+    while (prompt->buffer->buff[prompt->indice] == ' ' && prompt->indice > 0)
     {
         prompt->indice--;
         create_termcap("le");
     }
-    while (prompt->buffer->buff[prompt->indice] != ' ' && prompt->indice - 1 > 0)
+    while (prompt->buffer->buff[prompt->indice] != ' ' && prompt->indice > 0)
     {
         prompt->indice--;
         create_termcap("le");
